8fc037a13c7cffc4bf29ba768ef72288c5f9b87a
hidayathullah <hidayathullah@LAPTOP-T5M67454>
2021-06-16 10:21:47 +0530

    19th commit

---
+++ Swap of two numbers/src/program3/SwapNumbers.java
@@ -0,0 +1,18 @@
+package program3;
+
+public class SwapNumbers {
+	public static void main(String[] args) {
+		int a=44,b=99,temp;
+		System.out.println("before swap");
+		System.out.println("value of a: "+a);
+		System.out.println("value of b: "+b);
+		temp=a;
+		a=b;
+		b=temp;
+		System.out.println("after swap");
+		System.out.println("value of a:"+a);
+		System.out.println("value of b:"+b);
+		
+	}
+
+}
