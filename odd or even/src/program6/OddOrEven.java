6863107c271ca4fab0c9028e676a712f7d06ea1c
hidayathullah <hidayathullah@LAPTOP-T5M67454>
2021-06-16 10:16:04 +0530

    11th commit

---
+++ odd or even/src/program6/OddOrEven.java
@@ -0,0 +1,15 @@
+package program5;
+
+public class OddOrEven {
+	public static void main(String[] args) {
+		int i=10;
+		if(i%2==0) {
+			System.out.println("It is a even number "+i);
+		}
+			else {
+				System.out.println("It is odd number "+i);
+			}
+		}
+	}
+
+
