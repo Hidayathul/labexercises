93acb3e2462f63afe4e863168d1d3a62a10c2089
hidayathullah <hidayathullah@LAPTOP-T5M67454>
2021-06-16 10:02:37 +0530

    4th commit

---
+++ area of square/src/program14/AreaOfSquare.java
@@ -0,0 +1,14 @@
+package program14;
+import java.util.Scanner;
+public class AreaOfSquare {
+	public static void main(String[] args) {
+		Scanner a=new Scanner(System.in);
+		System.out.println("Enter the side of square:");
+		int side=a.nextInt();
+		double area=side*side;
+	System.out.println("Area of square:"+area);
+	
+		
+	}
+
+}
