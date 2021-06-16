0e65f5a9d78700d9c10b31ed24b1caf263a8acf6
hidayathullah <hidayathullah@LAPTOP-T5M67454>
2021-06-16 09:56:33 +0530

    third commit

---
+++ areaofrectangle/src/program13/AreaOfRectangle.java
@@ -0,0 +1,16 @@
+package program13;
+import java.util.Scanner;
+public class AreaOfRectangle {
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		int l,b;
+		System.out.println("Enter the length and breadth:");
+		l=in.nextInt();
+		b=in.nextInt();
+		int  area=l*b;
+		System.out.println("Area of recctangle:"+area);
+		
+				
+	}
+
+}
