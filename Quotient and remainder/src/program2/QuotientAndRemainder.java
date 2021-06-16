db5af54ee5a308aa1fa85493066f55ebb7ec1114
hidayathullah <hidayathullah@LAPTOP-T5M67454>
2021-06-16 10:18:23 +0530

    14th commit

---
+++ Quotient and remainder/src/program2/QuotientAndRemainder.java
@@ -0,0 +1,10 @@
+package program2;
+
+public class QuotientAndRemainder {
+	public static void main(String[] args) {
+		int a=55,b=44,Q=a/b,R=a%b;
+		System.out.println("Quotient "+Q);
+		System.out.println("Remainder "+R);
+	}
+
+}
